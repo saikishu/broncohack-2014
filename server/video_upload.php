be7f92c9ef37b596fad58dec6a2cd5e29710f8c0
Sai Kishore <saikishu@users.noreply.github.com>
2015-02-27 15:24:46 -0800

    Update video_upload.php

--- server/video_upload.php
+++ server/video_upload.php
@@ -4,7 +4,7 @@ $video_name = "video-".date("Y-m-d-i-s",time()).".mov";
 
 $gameId = trim(file_get_contents('http://'.$_SERVER['HTTP_HOST'].'/api.php?call=fetch_latest_game'));
 
-$dir = "/Users/srinivas/Projects/broncohack/server/uploads/videos/".$gameId."/";
+$dir = "/broncohack/server/uploads/videos/".$gameId."/";
 
 if (!file_exists($dir)) {
     mkdir($dir, 0777, true);
